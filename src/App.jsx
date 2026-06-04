31e47b2ebd676e9f0857f12cb97a63d499b165b0
Eugénie Adelin <eugenie.aimee@hotmail.fr>
2026-06-04 19:38:44 +0200

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './index.css'
 import { Routes, Route } from "react-router-dom"
-import HomePage from "../pages/HomePage"
-import NotFound from "../pages/NotFound"
-import Calendar from "../pages/Calendar"
-import FormPage from "../pages/FormPage"
-import About from "../pages/About"
-import Profile from "../pages/Profile"
-import VisionBoard from "../pages/VisionBoard"
-import WishListe from "../pages/WishList"
-import WishPage from "../pages/WishPage"
+import HomePage from "./pages/HomePage"
+import NotFound from "./pages/NotFound"
+import Calendar from "./pages/Calendar"
+import FormPage from "./pages/FormPage"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import VisionBoard from "./pages/VisionBoard"
+import WishListe from "./pages/WishList"
+import WishPage from "./pages/WishPage"
 function App() {
 
   return (
@@ -16,13 +16,13 @@ function App() {
       <div>Welcom on WishCraft</div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/pages/About' element={<AboutPage />} />
-        <Route path='/wishes' element={<WishPage />} />
-        <Route path='/' element={<WishListe />} />
-        <Route path='/' element={< />} />
-        <Route path='/' element={<About />} />
-        <Route path='/' element={<About />} />
-        <Route path='/pages/About.jsx' element={<About />} />
+        <Route path='/pages/About' element={<About />} />
+        <Route path='/wishes:id' element={<WishPage />} />
+        <Route path='/wishes' element={<WishListe />} />
+        <Route path='/create' element={<FormPage />} />
+        <Route path='/user' element={<Profile />} />
+        <Route path='/board' element={<VisionBoard />} />
+        <Route path='/calendar' element={<Calendar />} />
         <Route path='/' element={<NotFound />} />
       </Routes>
 
